8d90a709ed26580595bba37d19f2b3c3881aad6b
Dave <dwiper@idreaminteractive.com>
2024-10-01 06:26:42 -0400

    Update oil.lua

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -3,7 +3,7 @@ return {
     opts = {
         view_options = {
             -- Show files and directories that start with "."
-            show_hidden = false,
+            show_hidden = true,
             -- This function defines what is considered a "hidden" file
             is_hidden_file = function(name, bufnr)
               return vim.startswith(name, ".")
