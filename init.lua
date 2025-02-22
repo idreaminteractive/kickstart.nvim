2bd0a8636677387425010dd0e1b4fab5e5ee0a28
Dave Wiper <dwiper@idreaminteractive.com>
2025-02-22 16:48:00 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -677,6 +677,16 @@ require('lazy').setup({
           settings = {
             tailwindCSS = {
               -- includeLanguages = { heex = 'html' },
+              root_dir = require('lspconfig').util.root_pattern(
+                'tailwind.config.js',
+                'tailwind.config.ts',
+                'postcss.config.js',
+                'postcss.config.ts',
+                'package.json',
+                'node_modules',
+                '.git',
+                'mix.exs'
+              ),
               includeLanguages = {
                 elixir = 'html-eex',
                 eelixir = 'html-eex',
