cefa50b66c6192a024c7d21d54fe231bfbef4953
Dave Wiper <dwiper@idreaminteractive.com>
2025-02-22 15:30:59 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -676,36 +676,14 @@ require('lazy').setup({
             'mix.exs'
           ),
           settings = {
-            -- tailwindCSS = {
-            --   includeLanguages = { heex = 'html' },
-            -- },
-            tailwindcss = function(_, opts)
-              local tw = LazyVim.lsp.get_raw_config 'tailwindcss'
-              opts.filetypes = opts.filetypes or {}
-
-              -- Add default filetypes
-              vim.list_extend(opts.filetypes, tw.default_config.filetypes)
-
-              -- Remove excluded filetypes
-              --- @param ft string
-              opts.filetypes = vim.tbl_filter(function(ft)
-                return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
-              end, opts.filetypes)
-
-              -- Additional settings for Phoenix projects
-              opts.settings = {
-                tailwindCSS = {
-                  includeLanguages = {
-                    elixir = 'html-eex',
-                    eelixir = 'html-eex',
-                    heex = 'html-eex',
-                  },
-                },
-              }
-
-              -- Add additional filetypes
-              vim.list_extend(opts.filetypes, opts.filetypes_include or {})
-            end,
+            tailwindCSS = {
+              -- includeLanguages = { heex = 'html' },
+              includeLanguages = {
+                elixir = 'html-eex',
+                eelixir = 'html-eex',
+                heex = 'html-eex',
+              },
+            },
           },
         },
         -- rust_analyzer = {},
