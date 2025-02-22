85edbaa042ebd129191c6c7440a5deeda670f187
Dave Wiper <dwiper@idreaminteractive.com>
2025-02-22 16:50:47 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -1,46 +1,5 @@
 --[[
 
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
 Kickstart Guide:
 
   TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
@@ -673,20 +632,20 @@ require('lazy').setup({
           --   '.git',
           --   'mix.exs'
           -- ),
+          root_dir = require('lspconfig').util.root_pattern(
+            'tailwind.config.js',
+            'tailwind.config.ts',
+            'postcss.config.js',
+            'postcss.config.ts',
+            'package.json',
+            'node_modules',
+            '.git',
+            'mix.exs'
+          ),
           filetypes = { 'heex', 'html', 'ex', 'tsx' },
           settings = {
             tailwindCSS = {
               -- includeLanguages = { heex = 'html' },
-              root_dir = require('lspconfig').util.root_pattern(
-                'tailwind.config.js',
-                'tailwind.config.ts',
-                'postcss.config.js',
-                'postcss.config.ts',
-                'package.json',
-                'node_modules',
-                '.git',
-                'mix.exs'
-              ),
               includeLanguages = {
                 elixir = 'html-eex',
                 eelixir = 'html-eex',
