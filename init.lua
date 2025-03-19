3ce9e3a4a49221d195971dae1ac4fb72ce9c8564
Dave Wiper <dwiper@idreaminteractive.com>
2025-03-19 13:17:28 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -359,18 +359,7 @@ require('lazy').setup({
         -- },
         pickers = {
           find_files = {
-            find_command = {
-              'rg',
-              '--files',
-              '--hidden',
-              '--no-ignore-vcs',
-              '-g',
-              '!**/.git/*',
-              '-g',
-              '!**/node_modules/*',
-              '-g',
-              '!**/.repro/*', -- just to hide .repro rtp
-            },
+            hidden = true,
           },
         },
         extensions = {
