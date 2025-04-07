53ab656eca71a7cc40cada635da731ddcd2cc253
Dave Wiper <dwiper@idreaminteractive.com>
2025-04-07 22:24:51 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -621,12 +621,12 @@ require('lazy').setup({
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      require('lspconfig').gleam.setup {}
       local servers = {
         -- clangd = {},
         gopls = {},
         -- pyright = {},
         elixirls = {},
-        gleam = {},
         tailwindcss = {
           -- root_dir = require('lspconfig').util.root_pattern(
           --   'tailwind.config.js',
