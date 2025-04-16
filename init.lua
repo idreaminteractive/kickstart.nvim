ed2843ec1b74c30507922017aa886d0b45c7e25d
Dave Wiper <dwiper@idreaminteractive.com>
2025-04-16 23:14:37 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -678,7 +678,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
         --
-
+        svelte = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
