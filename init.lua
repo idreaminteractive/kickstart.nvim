617a6b5138406be065ed4c7875278a9b7b91cdb5
Dave Wiper <dwiper@idreaminteractive.com>
2025-04-19 16:22:15 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -747,42 +747,40 @@ require('lazy').setup({
         desc = '[F]ormat buffer',
       },
     },
-    opts = function()
-      return {
-        notify_on_error = false,
-        format_on_save = function(bufnr)
-          -- Disable "format_on_save lsp_fallback" for languages that don't
-          -- have a well standardized coding style. You can add additional
-          -- languages here or re-enable it for the disabled ones.
-          local disable_filetypes = { c = true, cpp = true }
-          local lsp_format_opt
-          if disable_filetypes[vim.bo[bufnr].filetype] then
-            lsp_format_opt = 'never'
-          else
-            lsp_format_opt = 'fallback'
-          end
-          return {
-            timeout_ms = 500,
-            lsp_format = lsp_format_opt,
-          }
-        end,
-        formatters_by_ft = {
-          lua = { 'stylua' },
-          yaml = { 'yamlfmt' },
-          sql = { 'sqlfmt' },
-          -- Conform can also run multiple formatters sequentially
-          -- python = { "isort", "black" },
-          --
-          -- You can use 'stop_after_first' to run the first available formatter from the list
-          -- javascript = { "prettierd", "prettier", stop_after_first = true },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        local lsp_format_opt
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          lsp_format_opt = 'never'
+        else
+          lsp_format_opt = 'fallback'
+        end
+        return {
+          timeout_ms = 500,
+          lsp_format = lsp_format_opt,
+        }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        yaml = { 'yamlfmt' },
+        sql = { 'sqlfmt' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      formatters = {
+        sqlfmt = {
+          command = 'bunx sql-formatter',
         },
-        formatters = {
-          sqlfmt = {
-            command = 'bunx sql-formatter',
-          },
-        },
-      }
-    end,
+      },
+    },
   },
 
   { -- Autocompletion
