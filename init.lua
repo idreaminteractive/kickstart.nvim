0fc2e0a01d7dbb4bfff2981a648c57ba48d7cf89
Dave Wiper <dwiper@idreaminteractive.com>
2025-04-19 04:09:42 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -769,15 +769,12 @@ require('lazy').setup({
         formatters_by_ft = {
           lua = { 'stylua' },
           yaml = { 'yamlfmt' },
-          sql = { 'sql_formatter' },
+          sql = { 'sqlfmt' },
           -- Conform can also run multiple formatters sequentially
           -- python = { "isort", "black" },
           --
           -- You can use 'stop_after_first' to run the first available formatter from the list
           -- javascript = { "prettierd", "prettier", stop_after_first = true },
-        },
-        formatters = {
-          sql_formatter = {},
         },
       }
     end,
