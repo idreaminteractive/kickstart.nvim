16eb92c5fc571b3fef044917c427a858f79e5ce4
Dave Wiper <dwiper@idreaminteractive.com>
2025-04-19 16:10:39 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -769,6 +769,7 @@ require('lazy').setup({
         formatters_by_ft = {
           lua = { 'stylua' },
           yaml = { 'yamlfmt' },
+          sql = { 'bunx sql-formatter' },
           -- Conform can also run multiple formatters sequentially
           -- python = { "isort", "black" },
           --
