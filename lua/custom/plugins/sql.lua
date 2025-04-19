d28f43a7f0aa8b890138e0531f34e220c43d4b5e
Dave Wiper <dwiper@idreaminteractive.com>
2025-04-19 04:04:20 +0000

    chore: iterating...

--- lua/custom/plugins/sql.lua
+++ lua/custom/plugins/sql.lua
@@ -1,6 +1,6 @@
 local cmp = require 'cmp'
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'sql',
+  pattern = '*.sql',
   callback = function()
     vim.api.nvim_create_autocmd('BufWritePre', {
       buffer = 0,
@@ -11,26 +11,26 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 -- Setup up vim-dadbod
-cmp.setup.filetype({ 'sql' }, {
-  sources = {
-    { name = 'vim-dadbod-completion' },
-    { name = 'buffer' },
-  },
-})
-return {
-  'kristijanhusak/vim-dadbod-ui',
-  dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
-  },
-  cmd = {
-    'DBUI',
-    'DBUIToggle',
-    'DBUIAddConnection',
-    'DBUIFindBuffer',
-  },
-  init = function()
-    -- Your DBUI configuration
-    vim.g.db_ui_use_nerd_fonts = 1
-  end,
-}
+-- cmp.setup.filetype({ 'sql' }, {
+--   sources = {
+--     { name = 'vim-dadbod-completion' },
+--     { name = 'buffer' },
+--   },
+-- })
+-- return {
+--   'kristijanhusak/vim-dadbod-ui',
+--   dependencies = {
+--     { 'tpope/vim-dadbod', lazy = true },
+--     { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+--   },
+--   cmd = {
+--     'DBUI',
+--     'DBUIToggle',
+--     'DBUIAddConnection',
+--     'DBUIFindBuffer',
+--   },
+--   init = function()
+--     -- Your DBUI configuration
+--     vim.g.db_ui_use_nerd_fonts = 1
+--   end,
+-- }
