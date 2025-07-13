65bf39d2498131a2ec43d62b1e236ec0d1f38709
Dave Wiper <dwiper@idreaminteractive.com>
2025-07-13 17:39:11 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -1015,6 +1015,16 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+  --
+  --
+  require('lazy').setup({
+    {
+      'supermaven-inc/supermaven-nvim',
+      config = function()
+        require('supermaven-nvim').setup {}
+      end,
+    },
+  }, {}),
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
