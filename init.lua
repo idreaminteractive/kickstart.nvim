ab9e09dd5e4b4f1b7351a358cbb66df43cd639c3
Dave Wiper <dwiper@idreaminteractive.com>
2025-07-13 17:46:54 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -1017,12 +1017,20 @@ require('lazy').setup({
   -- you can continue same window with `<space>sr` which resumes last telescope search
   --
   --
-    {
-      'supermaven-inc/supermaven-nvim',
-      config = function()
-        require('supermaven-nvim').setup {}
-      end,
-    },
+  {
+    'supermaven-inc/supermaven-nvim',
+    config = function()
+      require('supermaven-nvim').setup {
+
+        keymaps = {
+          accept_suggestion = '<C-l>',
+          clear_suggestion = '<C-]>',
+          accept_word = '<C-j>',
+        },
+        ignore_filetypes = { templ = true },
+      }
+    end,
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
