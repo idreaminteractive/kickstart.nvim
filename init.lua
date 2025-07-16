d40cfff2122a63848d169d0792496e3f762cd3ad
Dave Wiper <dwiper@idreaminteractive.com>
2025-07-16 02:20:02 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -1021,7 +1021,6 @@ require('lazy').setup({
     'supermaven-inc/supermaven-nvim',
     config = function()
       require('supermaven-nvim').setup {
-
         keymaps = {
           accept_suggestion = '<C-j>',
           clear_suggestion = '<C-]>',
@@ -1029,6 +1028,7 @@ require('lazy').setup({
         },
         ignore_filetypes = { templ = true },
       }
+      require('supermaven-nvim.api').use_free_version()
     end,
   },
 }, {
