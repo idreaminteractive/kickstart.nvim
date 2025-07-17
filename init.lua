2fdec908a003d63f9af6589782ca2bf9c484a2cf
Dave Wiper <dwiper@idreaminteractive.com>
2025-07-17 23:59:21 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -352,11 +352,11 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = { ['<C-j>'] = actions.move_selection_next },
+          },
+        },
         pickers = {
           find_files = {
             hidden = true,
