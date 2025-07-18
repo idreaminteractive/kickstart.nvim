aba5570d3c3baec1d586eb7358a5fed35af7043d
Dave Wiper <dwiper@idreaminteractive.com>
2025-07-18 01:12:24 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -76,19 +76,40 @@ vim.opt.showmode = false
 -- vim.schedule(function()
 --   vim.opt.clipboard = 'unnamedplus'
 -- end)
+--
 
+-- In your init.lua
+local function copy_to_clipboard(text)
+  local base64_text = vim.fn.system('echo -n "' .. text .. '" | base64 -w 0')
+  -- Remove any trailing newline
+  base64_text = base64_text:gsub('\n$', '')
+  -- Send OSC 52 sequence
+  io.stdout:write('\027]52;c;' .. base64_text .. '\007')
+end
+
+-- Set up clipboard
 vim.g.clipboard = {
   name = 'OSC 52',
   copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy '+',
-    ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+    ['+'] = function(lines, regtype)
+      copy_to_clipboard(table.concat(lines, '\n'))
+    end,
+    ['*'] = function(lines, regtype)
+      copy_to_clipboard(table.concat(lines, '\n'))
+    end,
   },
   paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').paste '+',
-    ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+    ['+'] = function(reg)
+      return vim.fn.getreg ''
+    end,
+    ['*'] = function(reg)
+      return vim.fn.getreg ''
+    end,
   },
 }
 
+-- Use system clipboard by default
+vim.opt.clipboard = 'unnamedplus'
 -- Enable break indent
 vim.opt.breakindent = true
 
