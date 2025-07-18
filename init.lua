231a9e7da4499e6850cd6e66ae27ebc24b0753c6
Dave Wiper <dwiper@idreaminteractive.com>
2025-07-18 00:12:47 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -1009,7 +1009,7 @@ require('lazy').setup({
   require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-  require('lazy').setup { { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' } },
+   { 'nvim-treesitter/nvim-treesitter-context'} ,
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
