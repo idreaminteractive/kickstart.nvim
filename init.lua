f5cb2c21be6354cb3ba79bc08de7a16a97d999a2
Dave Wiper <dwiper@idreaminteractive.com>
2025-08-28 01:17:07 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -21,14 +21,6 @@ vim.opt.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
--- vim.schedule(function()
---   vim.opt.clipboard = 'unnamedplus'
--- end)
 --
 vim.opt.winbar = '%f'
 -- In your init.lua
@@ -273,7 +265,6 @@ require('lazy').setup({
         { '<leader>d', group = '[D]ocument' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
-        -- { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
@@ -507,6 +498,18 @@ require('lazy').setup({
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+          map(
+            '<leader>cA',
+            vim.lsp.buf.code_action {
+              apply = true,
+              context = {
+                only = { 'source' },
+                diagnostics = {},
+              },
+            },
+            'Sour[C]e [A]ction',
+            { 'n', 'x' }
+          )
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
