accb36cb8163fb2486f9bf7fde8b6c8c0c16ea86
Dave Wiper <dwiper@idreaminteractive.com>
2025-08-28 00:57:46 +0000

    chore: iterating...

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -2,6 +2,14 @@ return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+    settings = {
+      save_on_toggle = true,
+    },
+  },
   config = function()
     local harpoon = require 'harpoon'
 
