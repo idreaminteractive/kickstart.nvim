70f87cb040d13cd0f399befa1ae83365c0c727f6
Dave Wiper <dwiper@idreaminteractive.com>
2025-08-28 01:04:51 +0000

    chore: iterating...

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -2,19 +2,15 @@ return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = {
-    menu = {
-      width = vim.api.nvim_win_get_width(0) - 4,
-    },
-    settings = {
-      save_on_toggle = true,
-    },
-  },
   config = function()
     local harpoon = require 'harpoon'
 
     -- REQUIRED
-    harpoon:setup()
+    harpoon:setup {
+      global_settings = {
+        ['save_on_toggle'] = true,
+      },
+    }
     -- REQUIRED
 
     vim.keymap.set('n', '<leader>a', function()
