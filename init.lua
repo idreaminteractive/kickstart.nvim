358ce1e3ffa95293135953ba13583ef48a1431d1
Dave Wiper <dwiper@idreaminteractive.com>
2025-08-28 01:00:18 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
---[[init
-
--- Set <space> as the leader key
--- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
