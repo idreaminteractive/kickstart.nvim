88dd1914ab6f9f1865c6ef3e4b7ef1ee94d3d778
Dave Wiper <dwiper@idreaminteractive.com>
2025-08-28 01:36:02 +0000

    chore: iterating...

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -8,7 +8,4 @@ return {
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
-  config = function()
-    vim.keymap.set('n', '<leader>e', '<CMD>Oil<CR>', { desc = 'Open Oil file explorer' })
-  end,
 }
