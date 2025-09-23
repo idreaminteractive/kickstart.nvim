7096f32a3c601dfdd09a842d0d8c8277a13e90dc
Dave Wiper <dwiper@idreaminteractive.com>
2025-09-23 16:55:24 +0000

    chore: iterating...

--- init.lua
+++ init.lua
@@ -334,14 +334,6 @@ require('lazy').setup({
         pickers = {
           find_files = {
             theme = 'dropdown',
-
-            layout_config = {
-              width = 0.8,
-              height = 0.7,
-              vertical = {
-                preview_width = 0.7,
-              },
-            },
             hidden = true,
             file_ignore_patterns = { '.git/' },
           },
