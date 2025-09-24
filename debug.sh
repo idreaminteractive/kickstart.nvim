417d682cc2bac7ef50721957f360bd561ba61ca1
Dave Wiper <dwiper@idreaminteractive.com>
2025-09-24 10:40:45 +0000

    chore: iterating...

--- debug.sh
+++ debug.sh
@@ -3,5 +3,6 @@
 git add . && git commit -am "chore: iterating..." && git push 
 cd /workspace/.config
 rm -rf nvim
-git clone https://github.com/idreaminteractive/kickstart.nvim nvim
+cp -R /workspace/kickstart.nvim ./nvim
+# git clone https://github.com/idreaminteractive/kickstart.nvim nvim
 cd /workspace/kickstart.nvim
