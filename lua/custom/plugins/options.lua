3f8164cea12e004ab55eaf16c6bf248d815f7745
Dave Wiper <dwiper@idreaminteractive.com>
2025-09-24 10:58:04 +0000

    chore: iterating...

--- lua/custom/plugins/options.lua
+++ lua/custom/plugins/options.lua
@@ -1,2 +1,12 @@
 vim.opt.relativenumber = true
+vim.keymap.set('n', '<leader>wh', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<leader>wl', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<leader>wj', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<leader>wk', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<leader>wq', '<C-w><C-q>', { desc = 'Close current window' })
+
+local actions = require 'telescope.actions'
+vim.keymap.set('i', '<C-j>', actions.move_selection_next)
+vim.keymap.set('i', '<C-k>', actions.move_selection_previous)
+
 return {}
