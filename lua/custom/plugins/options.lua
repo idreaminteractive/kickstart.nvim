613124a565cf3f598c135b39fa4d337b32ff94da
Dave Wiper <dwiper@idreaminteractive.com>
2025-09-24 11:02:07 +0000

    chore: iterating...

--- lua/custom/plugins/options.lua
+++ lua/custom/plugins/options.lua
@@ -5,8 +5,40 @@ vim.keymap.set('n', '<leader>wj', '<C-w><C-j>', { desc = 'Move focus to the lowe
 vim.keymap.set('n', '<leader>wk', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 vim.keymap.set('n', '<leader>wq', '<C-w><C-q>', { desc = 'Close current window' })
 
-local actions = require 'telescope.actions'
-vim.keymap.set('i', '<C-j>', actions.move_selection_next, { desc = 'Next' })
-vim.keymap.set('i', '<C-k>', actions.move_selection_previous, { desc = 'Previous' })
-
+-- local actions = require 'telescope.actions'
+-- vim.keymap.set('i', '<C-j>', actions.move_selection_next(), { desc = 'Next' })
+-- vim.keymap.set('i', '<C-k>', actions.move_selection_previous(), { desc = 'Previous' })
+--
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-j>'] = require('telescope.actions').move_selection_next,
+        ['<C-k>'] = require('telescope.actions').move_selection_previous,
+      },
+    },
+  },
+  pickers = {
+    find_files = {
+      theme = 'dropdown',
+      hidden = true,
+      file_ignore_patterns = { '.git/' },
+    },
+    live_grep = {
+      theme = 'dropdown',
+    },
+    buffers = {
+      theme = 'dropdown',
+    },
+    lsp_references = {
+      theme = 'dropdown',
+    },
+    lsp_definitions = {
+      theme = 'dropdown',
+    },
+    lsp_implementations = {
+      theme = 'dropdown',
+    },
+  },
+}
 return {}
